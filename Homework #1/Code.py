7bac9dee9b3499fef6996dec80329c383019e871
Mohammed Jasam <mnqnd@mst.edu>
2017-10-06 18:42:21 -0500

    Working on new parameters

--- Homework #1/Code.py
+++ Homework #1/Code.py
@@ -1,12 +1,4 @@
-import random
-
-# Randomly assigns weights to W0 and W1
-weight = []
-weight.append(random.random())
-weight.append(random.random())
-
-# Error list
-error = []
+from random import randint
 
 # Extract all the data from the file to list examples
 examples = []
@@ -15,16 +7,38 @@ with open("zdata.txt", "r") as f:
         line = line[:-1].split("\t")
         examples.append((int(line[0]), int(line[1])))
 
-for i in range(len(examples)):
-    error.append(0)
+# Randomly assigns weights to W0 and W1
+weight = []
+weight.append(randint(0, 20000))
+weight.append(randint(0, 20000))
 
-print(examples[-1][0])
+# Error list
+error = [0, 0]
+eta = 0.001
+
 # Running 1500 iterations
-X = [1,0] # Xi values!
-for i in range(1500):
+X = [1, 0] # Xi values!
+delta = 0
+for x in range(1500):
     for k in range(len(examples)):
         X[1] = examples[k][0]
         Y = examples[k][1]
         YCap = (weight[0] * X[0]) + (weight[1] * X[1])
-        error[k] = error[k] + (Y - YCap)*examples[-1][0]
-    
+        delta = (Y - YCap)
+
+        for i in range(2):
+            error[i] = error[i] + delta * X[i]
+
+    for i in range(2):
+        error[i] = (1 / len(examples)) * error[i]
+        weight[i] = weight[i] + eta * error[i]
+    # print(delta)
+    error = [0, 0]
+
+SSE = delta * delta
+print("CS-5001 : HW#1 : Regression with one variable.")
+print("Programmer : Mohammed Jasam")
+print("Learning Rate eta = 0.001")
+print("After 1500 iterations:")
+print("Sum of Squares Error = " + str(SSE))
+print("Weights:\nw0 = "+str(weight[0])+"\nw1 = "+str(weight[1]))
--- Homework #1/oldcode.py
+++ Homework #1/oldcode.py
@@ -8,8 +8,8 @@ with open("zdata.txt", "r") as f:
         s = line[:-1].split("\t")
         l.append((int(s[0]), int(s[1])))
 
-w0 = randint(1,101)
-w1 = randint(1,101)
+w0 = randint(1, 101)
+w1 = randint(1, 101)
 yErr = []
 for i in range(1500):
     for j in range(len(l)):
@@ -19,7 +19,7 @@ for i in range(1500):
         yCap = (w0 * 1) + (w1 * people)
         yE = y - yCap
         yErr.append(yE)
-    print(sum(yErr)/len(l))
+    print(sum(yErr) / len(l))
 
     for k in range(len(l)):
         w0 = w0 + eta * yErr[k] * 1
