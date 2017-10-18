995fc282c14752992ee3c83ba7a87bd7f4cda95b
Mohammed Jasam <mnqnd@mst.edu>
2017-10-18 00:17:16 -0500

    Need to analyse the SSE and Sigmoid Function

--- Homework #2/code.py
+++ Homework #2/code.py
@@ -1,55 +1,70 @@
+import math
 from random import randint
-# import matplotlib.pyplot as plt
+
+# Sigmoid Function
+def sigmoid(x):
+  return 1 / (1 + math.e**(-x))
+
 
 # Variable declarations
+X = []
 examples = []
-initialX, initialY, weight = [], [], []
-error = [0, 0]
+YCap = 0
+error = []
+eta = 0.05
 delta = 0
-eta = 0.001
-X = [1, 0]
 
-# Extract all the data from the file to list examples
-with open("zdata.txt", "r") as f:
+# Collect examples from the data
+with open("pizzacatdata.txt") as f:
     for line in f:
-        line = line[:-1].split("\t")
-        examples.append((int(line[0]), int(line[1])))
-        initialX.append(int(line[0]))
-        initialY.append(int(line[1]))
+        examples.append( ([int(x) for x in line[:-4].split()], int(line[-3])) )
 
-weight.append(randint(0, 20000))
-weight.append(randint(0, 20000))
+
+# Randomly assigned 5 weights
+# w = []
+# for i in range(5):
+#     w.append(randint(-100,100))
+# # w = [77, -45, -29, -41, -76]
+# print("Initial Weights")
+# print(w)
+# print()
 
 SSE = []
-# Running 1500 iterations
-for x in range(1500):
-    finalY = []
-    SSE = []
-    for k in range(len(examples)):
-        X[1] = examples[k][0]
-        Y = examples[k][1]
-        YCap = (weight[0] * X[0]) + (weight[1] * X[1])
-        delta = (Y - YCap)
-        finalY.append(YCap)
-        SSE.append(delta * delta)
-        for i in range(2):
-            error[i] = error[i] + delta * X[i]
+cSSE = []
+# Run Experiment 5000 times
+for ab in range(1000):
+    w = []
+    for i in range(5):
+        w.append(randint(-100,100))
+    iterations = 5000
+    for i in range(iterations):
+        YCap = 0
+        SSE = []
+        for e in examples:
+            X = [1] + e[0]
+            Y = e[1]
 
-    for i in range(2):
-        error[i] = (1 / len(examples)) * error[i]
-        weight[i] = weight[i] + eta * error[i]
-    error = [0, 0]
+            for j in range(5):
+                YCap += w[j]*X[j]
+            YCap = sigmoid(YCap)
+            p = YCap
 
-# Output Part
-print("CS-5001 : HW#1 : Regression with one variable.")
-print("Programmer : Mohammed Jasam\n\n")
-print("Learning Rate eta = 0.001")
-print("After 1500 iterations:")
-print("Sum of Squares Error = " + str(sum(SSE)))
-print("Weights:\nw0 = " + str(weight[0]) + "\nw1 = " + str(weight[1]))
+            delta = Y - YCap
+            SSE.append(delta**2)
 
-# Plotting Observed and Predicted Values
-# plt.plot(initialX, initialY, 'ro')
-# plt.plot(initialX, finalY, 'b')
-# plt.legend(['Observed Y', 'Predicted Y'], loc='upper left')
-# plt.show()
+            for index in range(len(w)):
+                w[index] = w[index] + eta * delta * p * (1 - p) * X[index]
+                # w[index] = w[index] + eta * delta * X[index]
+    cSSE.append(sum(SSE))
+    print(ab , sorted(cSSE)[0])
+
+# print(sorted(cSSE))
+# # Output Part
+# print("CS-5001 : HW#2 : Logistic Regression.")
+# print("Programmer : Mohammed Jasam")
+# print("No cats were hurt gathering this data.\n")
+# print("Learning Rate eta = "+str(eta))
+# print("After " + str(iterations) + " iterations:")
+# print("Sum of Squares Error = " + str(sum(SSE)))
+# print("Weights:\nw0 = " + str(w[0]) + "\nw1 = " + str(w[1]) + "\nw2 = " + str(w[2]) + "\nw3 = " + str(w[3]) + "\nw4 = " + str(w[4]))
+# print(w)
